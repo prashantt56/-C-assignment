7ad97f19c2cc9aa582b370f4aaf0ae20c371eb47
prashantt56 <prashant.singh1_cs24@gla.ac.in>
2025-04-30 20:57:02 +0530

    Update area.h

--- area.h
+++ area.h
@@ -1,11 +1,16 @@
-int tri(int x,int y){
-	return (x/y)*100;
-}
-int squ(int x,int y){
-	return x*x;
-	
-}
-int rec(int x, int y){
-	return x*y;
-}
+#ifndef SHAPES_H
+#define SHAPES_H
 
+
+int tri(int x, int y);
+int squ(int x, int y);
+int rec(int x, int y);
+
+typedef int (*ShapeFunc)(int, int);
+
+typedef struct {
+    const char* name;
+    ShapeFunc func;
+} Shape;
+
+#endif
