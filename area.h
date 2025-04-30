2c2bd4b0c2a7241ff3a283fda0b83c16517b71ce
prashantt56 <prashant.singh1_cs24@gla.ac.in>
2025-04-30 21:01:14 +0530

    Update area.h

--- area.h
+++ area.h
@@ -1,15 +1,11 @@
 #ifndef AREA_H
 #define AREA_H
-
 int tri(int x, int y);
 int squ(int x, int y);
 int rec(int x, int y);
-
 typedef int (*AreaFunc)(int, int);
-
-typedef struct {
+struct Area {
     const char* name;
     AreaFunc func;
-} Area;
-
-#endif 
+};
+#endif
